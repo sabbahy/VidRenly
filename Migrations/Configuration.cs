6b97e9527a794b08370ee50120687f09711afdc5
Ahmed Alsabbahy <ahmed.18476@eng.modern-academy.edu.eg>
2020-05-27 14:59:15 +0200

    version 0.1 - added database tables for (movies,customers, asp auth users) -ADDED : - changed bootstrap theme. - changes in controllers ( customers, movies) -changes in models ( customers, movies, identity models) -changes in viewmodels -changes in views of( home,customers,movies,shared)

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VidRenly.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VidRenly.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VidRenly.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
